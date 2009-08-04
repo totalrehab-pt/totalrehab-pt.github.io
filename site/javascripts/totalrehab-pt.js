674f9002c976834dd8cc7fdaef2e6c2febb76f0f
Ryan Sonnek <ryan@socialcast.com>
2009-08-03 21:10:36 -0500

    add crossfade images to sidebar

---
+++ site/javascripts/totalrehab-pt.js
@@ -0,0 +1,17 @@
+$(document).ready(function() {
+  $('#slideshow').crossSlide({
+    sleep: 4,
+    fade: 1
+  }, [
+    { src: 'images/restaffimages/athlete.gif' },
+    { src: 'images/restaffimages/bungeerunner.gif' },
+    { src: 'images/restaffimages/gymarea.gif' },
+    { src: 'images/restaffimages/gymarea2.gif' },
+    { src: 'images/restaffimages/Handtherapy.gif' },
+    { src: 'images/restaffimages/Handtherapy2.gif' },
+    { src: 'images/restaffimages/ladder.gif' },
+    { src: 'images/restaffimages/peds.gif' },
+    { src: 'images/restaffimages/receptionist.gif' }
+  ]);
+});
+
