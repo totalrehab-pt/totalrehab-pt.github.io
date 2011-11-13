3b397d461b9b57eddc96edfe8449d219b05b4a2a
Ryan Sonnek <ryan@socialcast.com>
2011-11-12 21:59:45 -0600

    fix cap deploy recipe

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,11 +1,12 @@
 set :application, "totalrehab-pt.com"
 set :scm, :none
+set :user, 'wireframe'
 
 role :web, "ftp.codecrate.com"
 
 namespace :deploy do
   task :default do
     `staticmatic build .`
-    upload "site", "/totalrehab-pt", :via=> :sftp, :recursive => true
+    top.upload "site", "/totalrehab-pt", :via=> :sftp, :recursive => true
   end
 end
